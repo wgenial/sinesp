fd8dc895ec9088c2d6546190231ddd537790c9d4
Giovani Generali <giovani.generali@gmail.com>
2019-01-16 18:17:50 -0200

    add padrao placa mercosul e melhorias

--- src/Sinesp.php
+++ src/Sinesp.php
@@ -4,130 +4,131 @@ namespace Sinesp;
 
 class Sinesp
 {
-    private $secret = '#8.1.0#g8LzUadkEHs7mbRqbX5l';
-    private $url = 'https://cidadao.sinesp.gov.br/sinesp-cidadao/mobile/consultar-placa/v4';
-    private $proxy = null;
-    private $timeout = 0;
+  private $secret = '#8.1.0#g8LzUadkEHs7mbRqbX5l';
+  private $url = 'https://cidadao.sinesp.gov.br/sinesp-cidadao/mobile/consultar-placa/v4';
+  private $proxy = null;
+  private $timeout = 0;
 
-    private $placa = '';
-    private $response = '';
-    private $dados = [];
+  private $placa = '';
+  private $response = '';
+  private $dados = [];
 
-    public function buscar($placa, array $proxy = [])
-    {
-        if ($proxy) {
-            $this->proxy($proxy['ip'], $proxy['porta']);
-        }
-
-        $this->setUp($placa);
-        $this->exec();
+  public function buscar($placa, array $proxy = [])
+  {
+    if ($proxy) {
+      $this->proxy($proxy['ip'], $proxy['porta']);
     }
 
-    public function dados()
-    {
-        return $this->dados;
+    $this->setUp($placa);
+    $this->exec();
+  }
+
+  public function dados()
+  {
+    return $this->dados;
+  }
+
+  public function proxy($ip, $porta)
+  {
+    $this->proxy = $ip . ':' . $porta;
+  }
+
+  public function timeout($timeout)
+  {
+    $this->timeout = $timeout;
+  }
+
+  public function __get($name)
+  {
+    return array_key_exists($name, $this->dados) ? $this->dados[$name] : '';
+  }
+
+  public function existe()
+  {
+    return array_key_exists('codigoRetorno', $this->dados) && $this->dados['codigoRetorno'] != '3';
+  }
+
+  private function exec()
+  {
+    $this->verificarRequisitos();
+    $this->obterResposta();
+    $this->tratarResposta();
+  }
+
+  private function obterResposta()
+  {
+    $xml = $this->xml();
+
+    $headers = array(
+      "Content-type: text/xml;charset=\"utf-8\"",
+      "Accept: text/xml",
+      "Cache-Control: no-cache",
+      "Pragma: no-cache",
+      "Content-length: ".strlen($xml),
+    );
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $this->timeout);
+    curl_setopt($ch, CURLOPT_TIMEOUT, $this->timeout);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_URL, $this->url);
+
+    if ($this->proxy) {
+      curl_setopt($ch, CURLOPT_PROXY, $this->proxy);
     }
 
-    public function proxy($ip, $porta)
-    {
-        $this->proxy = $ip . ':' . $porta;
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+    $this->response = curl_exec($ch);
+
+    curl_close($ch);
+  }
+
+  private function tratarResposta()
+  {
+    if (! $this->response) {
+      throw new \Exception('O servidor retornou nenhuma resposta!');
     }
 
-    public function timeout($timeout)
-    {
-        $this->timeout = $timeout;
+    $response = str_ireplace(['soap:', 'ns2:'], '', $this->response);
+
+    $this->dados = (array) simplexml_load_string($response)->Body->getStatusResponse->return;
+  }
+
+  private function verificarRequisitos()
+  {
+    if (! function_exists('curl_init')) {
+      throw new \Exception('Incapaz de processar. PHP requer biblioteca cURL');
     }
 
-    public function __get($name)
-    {
-        return array_key_exists($name, $this->dados) ? $this->dados[$name] : '';
+    if (! function_exists('simplexml_load_string')) {
+      throw new \Exception('Incapaz de processar. PHP requer biblioteca libxml');
     }
 
-    public function existe()
-    {
-        return array_key_exists('codigoRetorno', $this->dados) && $this->dados['codigoRetorno'] != '3';
+    return;
+  }
+
+  private function setUp($placa)
+  {
+    $placa = $this->ajustar($placa);
+
+    if (! $this->validar($placa)) {
+      throw new \Exception('Placa do veiculo nao especificada ou em formato invalido!');
     }
 
-    private function exec()
-    {
-        $this->verificarRequisitos();
-        $this->obterResposta();
-        $this->tratarResposta();
-    }
+    $this->placa = $placa;
+  }
 
-    private function obterResposta()
-    {
-        $xml = $this->xml();
+  private function token()
+  {
+    return hash_hmac('sha1', $this->placa, $this->placa . $this->secret);
+  }
 
-        $headers = array(
-            "Content-type: text/xml;charset=\"utf-8\"",
-            "Accept: text/xml",
-            "Cache-Control: no-cache",
-            "Pragma: no-cache",
-            "Content-length: ".strlen($xml),
-        );
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $this->timeout);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_URL, $this->url);
-
-        if ($this->proxy) {
-            curl_setopt($ch, CURLOPT_PROXY, $this->proxy);
-        }
-
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-        $this->response = curl_exec($ch);
-
-        curl_close($ch);
-    }
-
-    private function tratarResposta()
-    {
-        if (! $this->response) {
-            throw new \Exception('O servidor retornou nenhuma resposta!');
-        }
-
-        $response = str_ireplace(['soap:', 'ns2:'], '', $this->response);
-
-        $this->dados = (array) simplexml_load_string($response)->Body->getStatusResponse->return;
-    }
-
-    private function verificarRequisitos()
-    {
-        if (! function_exists('curl_init')) {
-            throw new \Exception('Incapaz de processar. PHP requer biblioteca cURL');
-        }
-
-        if (! function_exists('simplexml_load_string')) {
-            throw new \Exception('Incapaz de processar. PHP requer biblioteca libxml');
-        }
-
-        return;
-    }
-
-    private function setUp($placa)
-    {
-        $placa = $this->ajustar($placa);
-
-        if (! $this->validar($placa)) {
-            throw new \Exception('Placa do veiculo nao especificada ou em formato invalido!');
-        }
-
-        $this->placa = $placa;
-    }
-
-    private function token()
-    {
-        return hash_hmac('sha1', $this->placa, $this->placa . $this->secret);
-    }
-
-    private function xml()
-    {
-        $xml=<<<EOX
+  private function xml()
+  {
+    $xml=<<<EOX
 <?xml version="1.0" encoding="utf-8" standalone="yes" ?>
 <v:Envelope xmlns:v="http://schemas.xmlsoap.org/soap/envelope/">
 <v:Header>
@@ -150,26 +151,26 @@ class Sinesp
 </v:Body>
 </v:Envelope>
 EOX;
-        return sprintf($xml, $this->latitude(), $this->token(), $this->longitude(), strftime('%Y-%m-%d %H:%M:%S'), $this->placa);
-    }
+  return sprintf($xml, $this->latitude(), $this->token(), $this->longitude(), strftime('%Y-%m-%d %H:%M:%S'), $this->placa);
+}
 
-    private function validar($placa)
-    {
-        return preg_match('/^[a-zA-Z]{3}-?\d{4}$/i', $placa);
-    }
+  private function validar($placa)
+  {
+    return preg_match('/^[a-z]{3}-?\d[a-z0-9]{2}\d$/i', trim($placa));
+  }
 
-    private function ajustar($placa)
-    {
-        return str_replace(['-', ' '], '', $placa);
-    }
+  private function ajustar($placa)
+  {
+    return str_replace('-', '', trim($placa));
+  }
 
-    private function latitude()
-    {
-        return '-38.5' . rand(100000, 999999);
-    }
+  private function latitude()
+  {
+    return '-38.5' . rand(100000, 999999);
+  }
 
-    private function longitude()
-    {
-        return '-3.7' . rand(100000, 999999);
-    }
+  private function longitude()
+  {
+    return '-3.7' . rand(100000, 999999);
+  }
 }
